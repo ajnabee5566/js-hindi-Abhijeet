b1b3bc32b407ddb38ee5eeb0507e61d11140e99e
ajnabee5566 <117076502+ajnabee5566@users.noreply.github.com>
2024-06-14 11:19:25 +0000

    ummmmahhh

---
+++ 01-Basics/02_Basics/01_controlFlow.js
@@ -0,0 +1,14 @@
+// const balance = 1000
+// if(balance>500){
+//     console.log("piase hi paise bc")
+// }
+const userLogin = true;
+const debitCard =true;
+const loggedInFromGoggle = false
+// if(userLogin&& debitCard&&2==2){
+//     console.log("Allow to buy course")
+// }
+if(userLogin||loggedInFromGoggle){
+    console.log("Allow to buy course")
+
+}
