3ada81b1a170a1f8a8d868f5cd2dee2098026896
ajnabee5566 <117076502+ajnabee5566@users.noreply.github.com>
2024-06-14 11:31:17 +0000

    ummmm

--- 01-Basics/02_Basics/01_controlFlow.js
+++ 01-Basics/02_Basics/01_controlFlow.js
@@ -12,3 +12,28 @@ if(userLogin||loggedInFromGoggle){
     console.log("Allow to buy course")
 
 }
+//switch case
+let month =4 
+switch(month){
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+    case 5:
+        console.log("may");
+        break;
+    default :
+        console.log("loda lelo");
+        break;
+        
+        
+        
+}
