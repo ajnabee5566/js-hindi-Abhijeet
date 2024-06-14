a3bc10db9cccf8895b1ce3f6729d74a344f6ba16
ajnabee5566 <117076502+ajnabee5566@users.noreply.github.com>
2024-06-14 10:51:55 +0000

    h

---
+++ 01-Basics/01_CallStack.js
@@ -0,0 +1,12 @@
+let v1 =10
+let v2 =30
+function addNum(n1,n2){
+    let total=n1+n2;
+    return total;
+    console.log(total)
+}
+let result1 =  addNum(v1,v2);
+let result2 = addNum(100,50)
+
+console.log(result1)
+console.log(result2)
