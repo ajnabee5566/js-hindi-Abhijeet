21035e7aafea4f433e20f24e3f1194bc82e69263
ajnabee5566 <117076502+ajnabee5566@users.noreply.github.com>
2024-06-15 01:44:26 +0000

    hjkhk

---
+++ 01-Basics/02_Basics/03_WhileLoop.js
@@ -0,0 +1,14 @@
+// while(condition){
+
+// }
+let index =0;
+while(index<=10){
+console.log(`value of index is ${index}`);
+index++;
+}
+let MyArray = ["batman","supermaan","spiderman"]
+let arr = 0;
+while(arr < MyArray.length){
+    console.log(MyArray[arr]);
+    arr++;
+}
