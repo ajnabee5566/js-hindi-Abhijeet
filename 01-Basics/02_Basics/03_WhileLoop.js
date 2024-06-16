5164100c73887deca7e6c51c311aecba7c9ff8de
ajnabee5566 <117076502+ajnabee5566@users.noreply.github.com>
2024-06-16 16:41:23 +0000

    h

--- 01-Basics/02_Basics/03_WhileLoop.js
+++ 01-Basics/02_Basics/03_WhileLoop.js
@@ -1,14 +1,21 @@
 // while(condition){
 
 // }
-let index =0;
-while(index<=10){
-console.log(`value of index is ${index}`);
-index++;
-}
-let MyArray = ["batman","supermaan","spiderman"]
-let arr = 0;
-while(arr < MyArray.length){
-    console.log(MyArray[arr]);
-    arr++;
-}
+// let index =0;
+// while(index<=10){
+// console.log(`value of index is ${index}`);
+// index++;
+// }
+// let MyArray = ["batman","supermaan","spiderman"]
+// let arr = 0;
+// while(arr < MyArray.length){
+//     console.log(MyArray[arr]);
+//     arr++;
+// }
+//do while loop
+let score = 11
+do{
+    console.log(`Score is ${score}`);
+    score++
+
+}while(score<=10);
