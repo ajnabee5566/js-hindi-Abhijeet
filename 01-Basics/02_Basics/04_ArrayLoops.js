b95d7202a07f0204d64becd360cd338576461aa3
ajnabee5566 <117076502+ajnabee5566@users.noreply.github.com>
2024-06-17 14:01:46 +0000

    dgvszf

---
+++ 01-Basics/02_Basics/04_ArrayLoops.js
@@ -0,0 +1,15 @@
+//for of :Array specific loop hai ye 
+
+const arr = [1,2,3,4,5]
+for(const val of arr){
+    console.log(val);
+}
+
+const greetings = "hello bhaiya"
+for(const greet of greetings){
+    console.log(greet);
+}
+const obj = {name : "Abhi"}
+for(const val of obj){
+    console.log(val);
+}
