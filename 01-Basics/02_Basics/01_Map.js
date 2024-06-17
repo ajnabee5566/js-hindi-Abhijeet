46e7425d206c7b0c6ba9ffabdfad3858a5d5116d
ajnabee5566 <117076502+ajnabee5566@users.noreply.github.com>
2024-06-17 14:01:26 +0000

    fdsgb

---
+++ 01-Basics/02_Basics/01_Map.js
@@ -0,0 +1,15 @@
+const map =  new Map()
+map.set("in","india")
+map.set("ak","ak-47")
+console.log(map); 
+for (const [key,value] of map){
+    console.log(key,":-",value)
+}
+const objects = {
+    'G1':'nfs'
+    'G2':'gta'
+
+}
+for (const [key,value] of object){
+    console.log(key,":-",value)
+}
